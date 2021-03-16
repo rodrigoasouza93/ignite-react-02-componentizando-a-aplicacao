b28d6a8d15924dc2da182f2cbb261db9088322ea
Rodrigo Souza <rtz@certi.org.br>
2021-03-16 20:18:35 -0300

    Create Content component

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,40 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from './MovieCard';
+
+import '../styles/content.scss';
+import { Header } from './Header';
+
+interface MovieProps {
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+interface ContentProps {
+  movies: MovieProps[];
+  selectedGenre: GenreResponseProps;
+}
+
+export function Content({ movies, selectedGenre }: ContentProps) {
+  return (
+    <div className="container">
+      <Header title={selectedGenre.title} />
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 }
